d662c0bbb551f3326141f180355cebbb494a870b
Antunes Chacuma <antuneschacuma@gmail.com>
2025-04-17 22:03:59 +0100

    feat: initial setup

---
+++ src/main/java/com/donas/kitanda_java/KitandaJavaApplication.java
@@ -0,0 +1,13 @@
+package com.donas.kitanda_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitandaJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KitandaJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/donas/kitanda_java/KitandaJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donas.kitanda_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitandaJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
